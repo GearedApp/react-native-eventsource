5007810c6aa695b30af9cc47032049941286e080
Facundo Acosta <facundo@gearedapp.co.uk>
2020-11-12 14:06:36 +0000

    🔈 Add console log

--- src/EventSource.js
+++ src/EventSource.js
@@ -1,15 +1,12 @@
-'use strict';
+"use strict";
 
-var React = require('react-native');
-var {
-  NativeModules,
-  DeviceEventEmitter
-} = React;
+var React = require("react-native");
+var { NativeModules, DeviceEventEmitter } = React;
 
 var RNEventSource = NativeModules.RNEventSource;
 
-var EventSourceBase = require('./EventSourceBase');
-var EventSourceEvent = require('./EventSourceEvent');
+var EventSourceBase = require("./EventSourceBase");
+var EventSourceEvent = require("./EventSourceEvent");
 
 var EventSourceId = 0;
 var CLOSE_NORMAL = 1000;
@@ -44,42 +41,43 @@ class EventSource extends EventSourceBase {
   }
 
   _unregisterEvents(): void {
-    this._subs.forEach(e => e.remove());
+    this._subs.forEach((e) => e.remove());
     this._subs = [];
   }
 
   _registerEvents(id: number): void {
     this._subs = [
-      DeviceEventEmitter.addListener('eventsourceEvent', ev => {
+      DeviceEventEmitter.addListener("eventsourceEvent", (ev) => {
         if (ev.id !== id) {
           return;
         }
+        console.log("event >> ", ev);
         var event = new EventSourceEvent(ev.type, {
-          data: ev.message
+          data: ev.message,
         });
-        if( ev.type === 'message' && this.onmessage ) this.onmessage(event);
+        if (ev.type === "message" && this.onmessage) this.onmessage(event);
         this.dispatchEvent(event);
       }),
-      DeviceEventEmitter.addListener('eventsourceOpen', ev => {
+      DeviceEventEmitter.addListener("eventsourceOpen", (ev) => {
         if (ev.id !== id) {
           return;
         }
         this.readyState = this.OPEN;
-        var event = new EventSourceEvent('open');
+        var event = new EventSourceEvent("open");
         this.onopen && this.onopen(event);
         this.dispatchEvent(event);
       }),
-      DeviceEventEmitter.addListener('eventsourceFailed', ev => {
+      DeviceEventEmitter.addListener("eventsourceFailed", (ev) => {
         if (ev.id !== id) {
           return;
         }
-        var event = new EventSourceEvent('error');
+        var event = new EventSourceEvent("error");
         event.message = ev.message;
         this.onerror && this.onerror(event);
         this.dispatchEvent(event);
         this._unregisterEvents();
         this.close();
-      })
+      }),
     ];
   }
 }
